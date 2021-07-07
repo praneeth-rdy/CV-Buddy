fe0bf33cc8a179f1be0f6fd67f4d5dbd28d3b53a
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-07-08 00:46:55 +0530

    Added education section form and its related js

--- iitm/main.js
+++ iitm/main.js
@@ -1,76 +1,78 @@
-// Add event listener of enter and get the searched text
-// Make http request and get the search results
-// Display the search results in the dynamic part
+let edu_items = 0;
 
-let searchInputEl = document.getElementById("searchInput");
-let searchResultsEl = document.getElementById("searchResults");
-
-let spinner = document.getElementById("spinner");
-
-function createAndAppendSearchResult(result){
-
-    // Create result item
-    let resultItemEl = document.createElement("div");
-    resultItemEl.classList.add("result-item");
-    searchResultsEl.appendChild(resultItemEl);
-
-    // Create search result title
-    let { link, title, description } = result;
-    let titleEl = document.createElement("a");
-    titleEl.href = link;
-    titleEl.target = "_blank";
-    titleEl.textContent = title;
-    titleEl.classList.add("result-title");
-    resultItemEl.appendChild(titleEl);
-
-    // Create a break element
-    let titleBreakEl = document.createElement("br");
-    resultItemEl.appendChild(titleBreakEl);
-
-    // Create a url element
-    let urlEl = document.createElement("a");
-    urlEl.href = link;
-    urlEl.target = "_blank";
-    urlEl.textContent = link;
-    urlEl.classList.add("result-url");
-    resultItemEl.appendChild(urlEl);
-
-    // Create a break element
-    let linkBreakEl = document.createElement("br");
-    resultItemEl.appendChild(linkBreakEl);
-
-    // Create a description element
-    let descriptionEl = document.createElement("p");
-    descriptionEl.classList.add("link-description");
-    descriptionEl.textContent = description;
-    resultItemEl.appendChild(descriptionEl);
-
+function removeEdu(event) {
+    event.preventDefault();
+    const toRemove = event.target.getAttribute('data-remove');
+    document.querySelector(toRemove).remove();
 }
 
-function displayResults(search_results){
-    for(let result of search_results){
-        createAndAppendSearchResult(result);
-    }    
+function addEdu(event) {
+    event.preventDefault();
+    edu_items++;
+    const formRow = document.createElement('div');
+    formRow.classList.add('form-row');
+    formRow.setAttribute('id', `edu-row${edu_items}`);
+    let formGroup = document.createElement('div');
+    formGroup.classList.add('form-group', 'col-6', 'col-md-3');
+    let input = document.createElement('input');
+    input.classList.add('form-control');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', `program${edu_items}`);
+    input.setAttribute('placeholder', 'Program');
+    formGroup.appendChild(input);
+    formRow.appendChild(formGroup);
+
+    formGroup = document.createElement('div');
+    formGroup.classList.add('form-group', 'col-6', 'col-md-3');
+    input = document.createElement('input');
+    input.classList.add('form-control');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', `insti${edu_items}`);
+    input.setAttribute('placeholder', 'Institution');
+    formGroup.appendChild(input);
+    formRow.appendChild(formGroup);
+
+    formGroup = document.createElement('div');
+    formGroup.classList.add('form-group', 'col-5', 'col-md-2');
+    input = document.createElement('input');
+    input.classList.add('form-control');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', `grade${edu_items}`);
+    input.setAttribute('placeholder', 'Percentage or cgpa');
+    formGroup.appendChild(input);
+    formRow.appendChild(formGroup);
+
+    formGroup = document.createElement('div');
+    formGroup.classList.add('form-group', 'col-5', 'col-md-2');
+    input = document.createElement('input');
+    input.classList.add('form-control');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', `year${edu_items}`);
+    input.setAttribute('placeholder', 'Year');
+    formGroup.appendChild(input);
+    formRow.appendChild(formGroup);
+
+    let btnDiv = document.createElement('div');
+    btnDiv.classList.add('col-2', 'edu-action-container');
+    let btn = document.createElement('button');
+    btn.classList.add('btn', 'btn-success');
+    btn.setAttribute('id', 'add-edu');
+    btn.textContent = 'Add';
+    btn.addEventListener('click', addEdu);
+    btnDiv.appendChild(btn);
+    formRow.appendChild(btnDiv);
+
+    document.getElementById('edu-section').appendChild(formRow);
+
+    event.target.remove()
+
+    btn = document.createElement('button');
+    btn.classList.add('btn', 'btn-danger');
+    btn.setAttribute('id', 'remove-edu');
+    btn.setAttribute('data-remove', `#edu-row${edu_items - 1}`);
+    btn.textContent = 'Remove';
+    btn.addEventListener('click', removeEdu);
+    document.getElementById(`edu-row${edu_items - 1}`).querySelector('.edu-action-container').appendChild(btn);
 }
 
-function searchWikipedia(event) {
-    if (event.key === "Enter") {
-        searchResultsEl.textContent = "";
-        let searchInput = searchInputEl.value;
-        let url = "https://apis.ccbp.in/wiki-search?search=" + searchInput;
-        let options = {
-            method: "GET"
-        };
-        spinner.classList.toggle("d-none");
-        fetch(url, options).then(function (response){
-            return response.json();
-        }).then(function (jsonData){
-            let { search_results } = jsonData;
-            spinner.classList.toggle("d-none");
-            displayResults(search_results);
-        });
-        console.log();
-    }
-}
-
-searchInputEl.addEventListener("keydown", searchWikipedia);
+$('#add-edu').on('click', addEdu);
