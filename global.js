0a942a73f9f45170c56c3169e5aa0ee246b27a08
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-07-08 17:51:17 +0530

    KGP CV Template addition

---
+++ global.js
@@ -0,0 +1,32 @@
+
+
+let cv = document.getElementById('cv');
+
+function initCv() {
+    cvData = JSON.parse(localStorage.getItem('cvData'));
+    if(cvData){
+        cv.innerHTML = cvData;
+    }
+}
+
+initCv();
+
+$('#editCv').on('click', (event) => {
+    cv.setAttribute('contenteditable', 'true');
+});
+
+$('#saveCv').on('click', (event) => {
+    cv.removeAttribute('contenteditable');
+    localStorage.setItem('cvData', JSON.stringify(cv.innerHTML));
+});
+
+$('#printCv').on('click', (event) => {
+    cv.classList.add('cv-to-print');
+    window.print();
+    cv.classList.remove('cv-to-print');
+});
+
+$('#resetCv').on('click', (event) => {
+    localStorage.removeItem('cvData');
+    location.reload();
+});
